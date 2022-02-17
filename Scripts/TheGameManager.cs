2ccae97ad73cd883782ce82944655b1870714d7e
Hugo Fabris <hugomfabris@gmail.com>
2022-02-17 18:03:08 -0300

    Fourth commit

---
+++ Scripts/TheGameManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TheGameManager : MonoBehaviour
+{
+    public static TheGameManager instance;
+
+    [SerializeField] 
+    private GameObject[] characters;
+
+    private int _charIndex;
+    public int CharIndex
+    {
+        get { return _charIndex; }
+        set { _charIndex = value; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Gameplay")
+        {
+            Instantiate(characters[CharIndex]);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
